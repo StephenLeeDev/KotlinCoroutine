f5740f2f36d7cb2834bbb76c87ee19b389e0b921
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-03-30 22:27:18 +0900

    CREATE | Create Query Request Method

--- app/src/main/java/com/example/kotlincoroutine/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutine/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.kotlincoroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var retService: AlbumService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        retService = RetrofitInstance
+                .getRetrofitInstance()
+                .create(AlbumService::class.java)
+
+        getRequestWithQueryParameters()
+    }
+
+    private fun getRequestWithQueryParameters() {
+        val responseLiveData: LiveData<Response<Albums>> = liveData {
+            val response = retService.getSortedAlbums(3)
+            emit(response)
+        }
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+            if (albumsList != null) {
+                while (albumsList.hasNext()) {
+                    val albumsItem = albumsList.next()
+                    val result = " " + "Album Title : ${albumsItem.title}" + "\n" +
+                            " " + "Album id : ${albumsItem.id}" + "\n" +
+                            " " + "User id : ${albumsItem.userId}" + "\n\n\n"
+                    text_view.append(result)
+                }
+            }
+        })
     }
 }
